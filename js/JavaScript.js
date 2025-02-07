63a0a200436cb42f59436a44da539aa08e53a134
JEres89 <98741207+JEres89@users.noreply.github.com>
2025-02-07 12:34:21 +0100

    Implement product-list from API call to https://api.jsoning.com/mock/public/products
    Removed duplicate shop page.

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -18,22 +18,51 @@ var products;
 } 
 */
 function GetProducts() {
-	fetch(apiUrl)
+	if (!products) {
+		fetch(apiUrl, {
+			headers: {
+				"accept": "application/json",
+				"Cross-Origin-Resource-Policy": "cross-origin"
+			}
+		})
 		.then(response => response.json())
-		.then(result => products = result, () => products = "Product data unavailable");
-		/* .then(data => {
-			console.log(data);
-			let output = '<h2>Products</h2>';
-			data.forEach(function (product) {
-				output += `
-				<div>
-					<h3>${product.name}</h3>
-					<p>${product.description}</p>
-				</div>
-			`;
-			});
-			document.getElementById('output').innerHTML = output;
-		}); */
+		.then(result => products = result, () => products = "Product data unavailable")
+		.then(() => ShowProducts());
+	}
+	else {
+		ShowProducts();
+	}
+}
+
+function ShowProducts() {
+	let store = document.getElementById("shopdiv");
+	if (products === "Product data unavailable") {
+		let error = document.createElement("p");
+		error.innerHTML = products;
+		store.innerHTML = error;
+	}
+	else {
+		store.innerHTML = "";
+		for (var i = 0; i < products.length; i++) {
+			let product = products[i];
+			let productDiv = document.createElement("div");
+			productDiv.id = product.id;
+			productDiv.className = "product";
+			productDiv.innerHTML =
+				`<div class="product-image">
+				<img src="${product.image_url}" alt="${product.name}" />
+				</span>
+				<h3>${product.name}</h3>
+				<p>${product.description}</p>
+				<p>Price: $${product.price}</p>
+				<p>Category: ${product.category}</p>
+				<p>Stock: ${product.stock}</p>
+				<p>Rating: ${product.rating.rate} (${product.rating.count} reviews)</p>
+				<button onclick="AddToCart(${product.id})">Add to Cart</button>
+				</div>`;
+			store.appendChild(productDiv);
+		}
+	}
 }
 
 // For curl requests
@@ -59,8 +88,4 @@ function GetProducts() {
 //	};
 
 //	xhr.send(data);
-//	fetch('https://api.github.com/users/defunkt')
-//		.then(response => response.json())
-//		.then(data => console.log(data));
-
 //}
